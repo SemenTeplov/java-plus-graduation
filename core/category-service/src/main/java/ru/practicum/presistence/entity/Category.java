2b0fba26295f145372751f9e579973fb0b380b19
SemenTeplov <eburg-severca@mail.ru>
2026-05-09 19:39:03 +0500

    microservices: broken monolit on microservices

--- core/category-service/src/main/java/ru/practicum/presistence/entity/Category.java
+++ core/category-service/src/main/java/ru/practicum/presistence/entity/Category.java
@@ -1,5 +1,6 @@
 package main.java.ru.practicum.presistence.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,9 +10,11 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 @Entity
@@ -22,11 +25,14 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @Table(name = "categories")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+@ToString
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(name = "name", length = 50, nullable = false)
     String name;
 }
--- core/event-service/src/main/java/ru/practicum/mapper/EventMapper.java
+++ core/event-service/src/main/java/ru/practicum/mapper/EventMapper.java
@@ -28,6 +28,9 @@ public interface EventMapper {
 
     @Mapping(target = "eventDate", qualifiedByName = "toOffsetDateTime")
     @Mapping(target = "location", ignore = true)
+    @Mapping(target = "paid", expression = "java(newEventDto.paid == null ? false : newEventDto.paid)")
+    @Mapping(target = "participantLimit", expression = "java(newEventDto.participantLimit == null ? 0 : newEventDto.participantLimit)")
+    @Mapping(target = "requestModeration", expression = "java(newEventDto.requestModeration == null ? false : newEventDto.requestModeration)")
     Event newEventDtoToEvent(NewEventDto newEventDto);
 
     @Mapping(target = "category", ignore = true)
