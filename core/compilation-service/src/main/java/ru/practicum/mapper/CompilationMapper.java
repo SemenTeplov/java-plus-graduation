9700ed7a70f33358bfb4e83d1de9135fdf594974
SemenTeplov <eburg-severca@mail.ru>
2026-05-10 23:47:44 +0500

    microservices: broken monolit on microservices

--- core/compilation-service/src/main/java/ru/practicum/mapper/CompilationMapper.java
+++ core/compilation-service/src/main/java/ru/practicum/mapper/CompilationMapper.java
@@ -26,7 +26,7 @@ public interface CompilationMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "events", source = "list")
-    @Mapping(target = "pinned", source = "pinned", defaultValue = "false")
+    @Mapping(target = "pinned", source = "newCompilationDto.pinned", defaultValue = "false")
     Compilation newCompilationDtoToCompilation(RequestCompilationDto newCompilationDto, List<Long> list);
 
     @Mapping(target = "id", ignore = true)
