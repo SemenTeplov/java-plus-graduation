abe547c4fdd4a4f48e100cabff304cad1e0ef0e4
SemenTeplov <eburg-severca@mail.ru>
2026-05-17 23:41:33 +0500

    recommendations: add recommendations

--- telemetry/collector/src/main/java/ru/practicum/Main.java
+++ telemetry/collector/src/main/java/ru/practicum/Main.java
@@ -2,9 +2,9 @@ package main.java.ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
