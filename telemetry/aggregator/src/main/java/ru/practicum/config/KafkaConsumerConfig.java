6083f05c091e7431c37f126475e3842da58a3b53
SemenTeplov <eburg-severca@mail.ru>
2026-05-18 11:36:09 +0500

    recommendations: add recommendations

--- telemetry/aggregator/src/main/java/ru/practicum/config/KafkaConsumerConfig.java
+++ telemetry/aggregator/src/main/java/ru/practicum/config/KafkaConsumerConfig.java
@@ -46,12 +46,12 @@ public class KafkaConsumerConfig {
         Map<String, Object> configs = new HashMap<>();
 
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UserActionDeserializer.class);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, String.class);
         configs.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, sessionTimeout);
         configs.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, heartbeatInterval);
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, BaseAvroDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, UserActionDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(configs);
     }
