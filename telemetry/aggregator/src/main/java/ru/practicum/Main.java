6bd5f17dc121c164dc9f4a5610518c2474e8c26b
SemenTeplov <eburg-severca@mail.ru>
2026-05-19 19:36:34 +0500

    recommendations: add recommendations

--- telemetry/aggregator/src/main/java/ru/practicum/Main.java
+++ telemetry/aggregator/src/main/java/ru/practicum/Main.java
@@ -3,9 +3,7 @@ package main.java.ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Main {
--- telemetry/aggregator/src/main/java/ru/practicum/starter/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/practicum/starter/AggregationStarter.java
@@ -11,15 +11,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import ru.practicum.ewm.stats.avro.EventSimilarityAvro;
 import ru.practicum.ewm.stats.avro.UserActionAvro;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Slf4j
 @Component
@@ -28,39 +23,23 @@ public class AggregationStarter {
 
     private final AggregatorService service;
 
-    private final CopyOnWriteArrayList<UserActionAvro> sensors = new CopyOnWriteArrayList<>();
-
     private final KafkaTemplate<String, EventSimilarityAvro> template;
 
     @Value("${kafka.topics.events}")
     private String eventTopic;
-
-    @Scheduled(fixedDelay = Values.FIXED_DELAY)
-    public void sendSnapshots() {
-
-        log.info(Message.GET_LIST_OF_SENSORS, sensors);
-
-        List<UserActionAvro> temp = new ArrayList<>(sensors);
-
-        for (UserActionAvro sensor : temp) {
-
-           service.updateState(sensor).ifPresent(list -> {
-
-               log.info(Message.SEND_LIST, list);
-
-               list.forEach(userActionAvro -> template.send(eventTopic, userActionAvro));
-           });
-
-            sensors.remove(sensor);
-        }
-    }
 
     @KafkaListener(topics = "${kafka.topics.user}", containerFactory = Values.EVENT_CONSUMER)
     public void handler(UserActionAvro event, Acknowledgment acknowledgment) {
 
         log.info(Message.GET_USER_ACTION_FROM_KAFKA, Values.EVENT_CONSUMER, event);
 
-        sensors.addIfAbsent(event);
+        service.updateState(event).ifPresent(list -> {
+
+            log.info(Message.SEND_LIST, list);
+
+            list.forEach(userActionAvro -> template.send(eventTopic, userActionAvro));
+        });
+
         acknowledgment.acknowledge();
     }
 }
