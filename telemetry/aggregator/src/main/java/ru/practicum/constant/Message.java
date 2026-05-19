e0e9d13f354417fc8c5cadf5987d69f9bad61a90
SemenTeplov <eburg-severca@mail.ru>
2026-05-19 19:38:30 +0500

    recommendations: add recommendations

--- telemetry/aggregator/src/main/java/ru/practicum/constant/Message.java
+++ telemetry/aggregator/src/main/java/ru/practicum/constant/Message.java
@@ -5,7 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Message {
 
-    public static final String GET_LIST_OF_SENSORS = "Получен список пользователей для отправки {}";
     public static final String SEND_LIST = "На отправку уходит следующий списсок {}";
     public static final String GET_USER_ACTION_FROM_KAFKA = "Из топика {} прибыло пользовательское действие {}";
     public static final String GET_LIST_OF_USER = "Получен список пользователей {}";
--- telemetry/aggregator/src/main/java/ru/practicum/constant/Values.java
+++ telemetry/aggregator/src/main/java/ru/practicum/constant/Values.java
@@ -5,7 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Values {
 
-    public static final long FIXED_DELAY = 5000;
     public static final String EVENT_CONSUMER = "eventConsumer";
-    public static final String USER_CONSUMER = "userConsumer";
 }
