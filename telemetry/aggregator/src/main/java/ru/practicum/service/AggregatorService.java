c5171d43f75ab8688cdf5b0d0d56957a206488df
SemenTeplov <eburg-severca@mail.ru>
2026-05-23 20:32:47 +0500

    recommendations: add recommendations

--- telemetry/aggregator/src/main/java/ru/practicum/service/AggregatorService.java
+++ telemetry/aggregator/src/main/java/ru/practicum/service/AggregatorService.java
@@ -84,7 +84,7 @@ public class AggregatorService {
             }
         }
 
-        return Optional.of(similarities);
+        return similarities.isEmpty() ? Optional.empty() : Optional.of(similarities);
     }
 
     private Integer[] compareEvents(Integer eventA, Integer eventB) {
