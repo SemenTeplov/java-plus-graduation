458eb5060cb7c5384fd841152fa5c3ede665324a
SemenTeplov <eburg-severca@mail.ru>
2026-05-24 18:33:00 +0500

    recommendations: add recommendations

--- stats/analyzer/src/main/java/ru/practicum/constant/Values.java
+++ stats/analyzer/src/main/java/ru/practicum/constant/Values.java
@@ -7,5 +7,4 @@ public class Values {
 
     public static final String USER_CONSUMER = "userConsumer";
     public static final String EVENT_CONSUMER = "eventsConsumer";
-    public static final long FIXED_DELAY = 5000;
 }
--- stats/analyzer/src/main/java/ru/practicum/listener/UserActionListener.java
+++ stats/analyzer/src/main/java/ru/practicum/listener/UserActionListener.java
@@ -13,14 +13,9 @@ import main.java.ru.practicum.persistence.status.ActionType;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import ru.practicum.ewm.stats.avro.UserActionAvro;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Slf4j
 @Service
@@ -31,36 +26,22 @@ public class UserActionListener {
 
     private final UserActionMapper userActionMapper;
 
-    private final CopyOnWriteArrayList<UserActionAvro> actions = new CopyOnWriteArrayList<>();
-
-    @Scheduled(fixedDelay = Values.FIXED_DELAY)
-    public void saveAction() {
-
-        List<UserActionAvro> forRemove = new ArrayList<>(actions);
-
-        for (UserActionAvro action : forRemove) {
-
-            log.info(Message.GET_USER_ACTION, action);
-
-            UserAction userAction = userActionMapper.toUserAction(action);
-
-            UserAction oldUserAction = userActionRepository.findById(new UserActionId(
-                    (long) action.getUserId(), (long) action.getEventId())).orElse(null);
-
-            if (oldUserAction == null || ActionType.getValue(oldUserAction.getActionType().name())
-                    < ActionType.getValue(userAction.getActionType().name())) {
-
-                userActionRepository.save(userAction);
-            }
-        }
-
-        actions.removeAll(forRemove);
-    }
-
     @KafkaListener(topics = "${kafka.topics.user}", containerFactory = Values.USER_CONSUMER)
     public void handler(UserActionAvro action, Acknowledgment acknowledgment) {
 
-        actions.add(action);
+        log.info(Message.GET_USER_ACTION, action);
+
+        UserAction userAction = userActionMapper.toUserAction(action);
+
+        UserAction oldUserAction = userActionRepository.findById(new UserActionId(
+                (long) action.getUserId(), (long) action.getEventId())).orElse(null);
+
+        if (oldUserAction == null || ActionType.getValue(oldUserAction.getActionType().name())
+                < ActionType.getValue(userAction.getActionType().name())) {
+
+            userActionRepository.save(userAction);
+        }
+
         acknowledgment.acknowledge();
     }
 }
